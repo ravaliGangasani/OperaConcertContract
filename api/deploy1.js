694c77d4211d92c64331791b1f2fcbe9b59eecc2
Ravali <ravali@cosmiclabs.co>
2020-05-14 10:32:53 +0530

    added api/deploy.js for operaconsert

---
+++ api/deploy1.js
@@ -0,0 +1,122 @@
+// @ts-check
+// Agoric Dapp api deployment script
+
+import fs from 'fs';
+import installationConstants from '../ui/public/conf/installationConstants.js';
+import {E} from '@agoric/eventual-send';
+import harden from '@agoric/harden';
+import {makeGetInstanceHandle} from '@agoric/zoe/src/clientSupport';
+import makeAmountMath from '@agoric/ertp/src/amountMath';
+
+
+export default async function deployApi(referencesPromise, {bundleSource, pathResolve}) {
+
+    // Let's wait for the promise to resolve.
+    const references = await referencesPromise;
+
+    // Unpack the references.
+    const {
+        wallet,
+        uploads: scratch,
+        spawner,
+        zoe,
+        registry,
+        http,
+    } = references;
+
+    const {
+        INSTALLATION_REG_KEY,
+    } = installationConstants;
+    const movieContractInstallationHandle = await E(registry).get(INSTALLATION_REG_KEY);
+
+    const issuersArray = await E(wallet).getIssuers();
+    const issuers = new Map(issuersArray);
+    const moolaIssuer = issuers.get('moola');
+
+    const getLocalAmountMath = issuer =>
+        Promise.all([
+            E(issuer).getBrand(),
+            E(issuer).getMathHelpersName(),
+        ]).then(([brand, mathHelpersName]) => makeAmountMath(brand, mathHelpersName));
+
+    const moolaAmountMath = await getLocalAmountMath(moolaIssuer);
+    const expectedAmount = moolaAmountMath.make(22)
+    console.log(expectedAmount)
+
+    const issuerKeywordRecords = harden({Money: moolaIssuer});
+    const adminInvite = await E(zoe).makeInstance(movieContractInstallationHandle, issuerKeywordRecords, {
+        show: 'Steven Universe, the Opera',
+        start: 'Web, March 25th 2020 at 8pm',
+        count: 3,
+        expectedAmount,
+    });
+    console.log('- SUCCESS! contract instance is running on Zoe');
+
+    const inviteIssuer = await E(zoe).getInviteIssuer();
+    const getInstanceHandle = makeGetInstanceHandle(inviteIssuer);
+    const instanceHandle = await getInstanceHandle(adminInvite);
+
+    const {publicAPI, terms} = await E(zoe).getInstanceRecord(instanceHandle);
+    console.log("publicAPI: ", publicAPI)
+
+    const pursesArray = await E(wallet).getPurses();
+    const purses = new Map(pursesArray);
+
+    const moolaPurse = purses.get('Fun budget');
+    console.log("moolaPurse: Alice", moolaPurse)
+    
+    const invite = await E(publicAPI).makeBuyerInvite()
+    const aliceInvite = await E(inviteIssuer).claim(invite)
+
+    const availableTickets = await E(publicAPI).getAvailableTickets()
+    console.log(availableTickets)
+
+
+    const {
+        payout: adminPayoutP,
+        outcome: adminOutcomeP,
+        cancelObj,
+    } = await E(zoe).offer(adminInvite);
+
+    const outcome = await adminOutcomeP;
+    console.log(`-- ${outcome}`);
+
+
+    const INSTANCE_REG_KEY = await E(registry).register(`${installationConstants.CONTRACT_NAME}instance`, instanceHandle);
+
+    console.log(`-- Contract Name: ${installationConstants.CONTRACT_NAME}`);
+    console.log(`-- InstanceHandle Register Key: ${INSTANCE_REG_KEY}`);
+
+    const {source, moduleFormat} = await bundleSource(pathResolve('./src/handler.js'));
+
+    // Install it on the spawner
+    const handlerInstall = E(spawner).install(source, moduleFormat);
+
+    // Spawn the running code
+    const brandPs = [];
+    const keywords = [];
+    Object.entries(issuerKeywordRecord).map(async ([keyword, issuer]) => {
+        keywords.push(keyword);
+        brandPs.push(E(issuer).getBrand());
+    });
+
+    const handler = E(handlerInstall).spawn({registry, brandPs, keywords, publicAPI});
+
+    await E(http).registerAPIHandler(handler);
+
+
+    // Re-save the constants somewhere where the UI and api can find it.
+    const dappConstants = {
+        INSTANCE_REG_KEY,
+        // BRIDGE_URL: 'agoric-lookup:https://local.agoric.com?append=/bridge',
+        BRIDGE_URL: 'http://127.0.0.1:8000',
+        API_URL: 'http://127.0.0.1:8000',
+    };
+    const defaultsFile = pathResolve(`../ui/public/conf/defaults.js`);
+    console.log('writing', defaultsFile);
+    const defaultsContents = `\
+  // GENERATED FROM ${pathResolve('./deploy.js')}
+  export default ${JSON.stringify(dappConstants, undefined, 2)};
+  `;
+    await fs.promises.writeFile(defaultsFile, defaultsContents);
+}
